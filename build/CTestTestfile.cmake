e280ea9b3829fb8a6e1f67296f7c2cb0fc80b9a2
Justin Nguyen <2014justinnguyen@gmail.com>
2018-12-21 14:13:43 -0600

    Removed files no longer tracked due to gitignore update

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justin/Github/ROS_Tutorials/src
-# Build directory: /home/justin/Github/ROS_Tutorials/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(beginner_tutorials)
--- build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justin/Github/ROS_Tutorials/src/beginner_tutorials
-# Build directory: /home/justin/Github/ROS_Tutorials/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
